46217235589c193f9e4761669a65922eeaa6d7de
bDrwx <a.bodosov@gmail.com>
2023-01-09 13:09:18 +0300

    Fix test for qsort

--- rdp_testcase/testcase1/sort.py
+++ rdp_testcase/testcase1/sort.py
@@ -12,5 +12,5 @@ def qsort(array):
     else:
         pivot = array[0]
         less = [i for i in array[1:] if i <= pivot]
-        greater = [i for i in array[1:] if i >= pivot]
+        greater = [i for i in array[1:] if i > pivot]
         return qsort(less) + [pivot] + qsort(greater)
--- tests/test_qsort.py
+++ tests/test_qsort.py
@@ -1,11 +1,14 @@
+import random
+
 import pytest
+
 from rdp_testcase.testcase1.sort import qsort
 
 
 @pytest.fixture
 def get_array():
-    return [10, 1, 2, 56, 34, 2, 3, 7, 9, 99, 234, 75, 45, 888]
+    return random.sample(range(-100, 100), 100)
 
 
 def test_qsort(get_array):
-    assert qsort(get_array) == [1, 2, 2, 2, 3, 7, 9, 10, 34, 45, 56, 75, 99, 234, 888]
+    assert sorted(get_array) == qsort(get_array)
