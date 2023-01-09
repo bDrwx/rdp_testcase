c355fc0f81227e6ee358e6bce3c87d5a60ed4f2d
bDrwx <a.bodosov@gmail.com>
2023-01-09 14:29:18 +0300

    Add some test

--- tests/test_qsort.py
+++ tests/test_qsort.py
@@ -6,9 +6,12 @@ from rdp_testcase.testcase1.sort import qsort
 
 
 @pytest.fixture
-def get_array():
+def get_list():
     return random.sample(range(-100, 100), 100)
 
 
-def test_qsort(get_array):
-    assert sorted(get_array) == qsort(get_array)
+def test_qsort(get_list):
+    assert sorted(get_list) == qsort(get_list)
+
+def test_one_element_list():
+    assert [1] == qsort([1])
